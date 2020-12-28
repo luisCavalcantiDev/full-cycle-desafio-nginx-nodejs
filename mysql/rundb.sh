a6cb83e9d6131c1c1a3e0696940fb765cb665558
luisCavalcantiDev <luis.cavalcanti583@gmail.com>
2020-12-28 17:15:40 -0300

    incluído mysql

---
+++ mysql/rundb.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+mysql -uroot < /tmp/setup.sql
--- nodeapp/index.js
+++ nodeapp/index.js
@@ -1,6 +1,36 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql');
+
+const config = {
+  host: 'nodeappdb',
+  user: 'root',
+  password: 'root',
+  database: 'nodeappdb'
+};
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-app.listen(3000, () => console.log('Server is up and running'));
+  res.send('<h1>Full Cycle Rocks!</h1>')
+});
+
+app.listen(3000, () => {
+  //createDatabase();
+  //createTable();
+
+  console.log('Server is up and running');
+});
+
+function createTable() {
+  const connection = mysql.createConnection(config);
+  const sql = `create table people(id int not null auto_increment, name varchar(255), primary key(id));`;
+  connection.query(sql);
+  connection.end();
+}
+
+function createDatabase() {
+  const connection = mysql.createConnection(config);
+  const sql = `create database nodeappdb;`;
+  connection.query(sql);
+  connection.end();
+}
+
